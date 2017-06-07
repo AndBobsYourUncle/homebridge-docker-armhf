85595b867847c34befa4e88eb6cff135f6389797
Nicholas Page <npage85@gmail.com>
2017-06-07 15:45:21 -0700

    Remove pid before container starts.

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ sed -i "s/rlimit-nproc=3/#rlimit-nproc=3/" /etc/avahi/avahi-daemon.conf
 sed -i "s/HOME_ASSISTANT_URL/"$(echo $HOME_ASSISTANT_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"/" /root/.homebridge/config.json
 sed -i "s/HOME_ASSISTANT_PASSWORD/"$(echo $HOME_ASSISTANT_PASSWORD | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"/" /root/.homebridge/config.json
 
+rm /var/run/dbus/pid
+
 dbus-daemon --system
 avahi-daemon -D
 
