6bdccf267abcb159f5ba4cd3b6a0b76b8c7d09e0
Nicholas Page <npage85@gmail.com>
2017-06-08 18:25:46 -0700

    Ignore if file doesn't exist.

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ sed -i "s/rlimit-nproc=3/#rlimit-nproc=3/" /etc/avahi/avahi-daemon.conf
 sed -i "s/HOME_ASSISTANT_URL/"$(echo $HOME_ASSISTANT_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"/" /root/.homebridge/config.json
 sed -i "s/HOME_ASSISTANT_PASSWORD/"$(echo $HOME_ASSISTANT_PASSWORD | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')"/" /root/.homebridge/config.json
 
-rm -f /var/run/dbus/pid
+rm -f /var/run/dbus/pid || true
 
 dbus-daemon --system
 avahi-daemon -D
